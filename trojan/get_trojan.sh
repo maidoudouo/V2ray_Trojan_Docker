220ad4b0b26eb76d3e2286c5e2e13f5722d542ee
DLGum <45528130+DLGum@users.noreply.github.com>
2020-09-13 16:43:05 +0800

    Update get_trojan.sh

--- trojan/get_trojan.sh
+++ trojan/get_trojan.sh
@@ -16,13 +16,13 @@ normalizeVersion() {
 }
 
 main(){
-    TAG_URL="https://api.github.com/repos/trojan-gfw/trojan/releases/latest"
+    TAG_URL="https://api.github.com/repos/p4gefau1t/trojan-go/releases/latest"
     VERSION="$(normalizeVersion "$(curl -s "${TAG_URL}" --connect-timeout 10| grep 'tag_name' | cut -d\" -f4)")"
 	if [[ $VERSION == "" ]]; then
             echo "Failed to fetch release information. Please check your network or try again."
             return
 	fi
 	echo "Latest trojan version:" $VERSION
-    git clone --branch=${VERSION} https://github.com/trojan-gfw/trojan.git
+    git clone --branch=${VERSION} https://github.com/p4gefau1t/trojan-go.git
 }
 main
